606cfc899c897509b6d6427f2f9f922376d62669
Roman Sarker <38065006+RomanSarker@users.noreply.github.com>
2018-12-31 18:52:38 +0600

    Add files via upload

---
+++ Testtclass.java
@@ -0,0 +1,14 @@
+public class Testtclass {
+
+    public static void main(String[] args) {
+        int i;
+        int a[] = new int[5];
+        a[0] = 50;
+        a[1] = 100;
+        a[2] = 200;
+        a[3] = 500;
+        a[4] = 600;
+        for (i = 0; i < a.length; i++)
+            System.out.println(a[i]);
+    }
+}
